b3a5af7fc301b70764d9ca850bb6fb2b6744b0e6
c0a25276 <c0a25276c7.edu.teu.ac.jp>
2026-06-23 16:16:07 +0900

    追加機能１完了

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -7,6 +7,7 @@ import pygame as pg
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
+NUM_OF_BOMBS = 5
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -138,6 +139,29 @@ class Bomb:
             self.vy *= -1
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
+        
+class Score:
+    """
+    スコアに関するクラス
+    """
+    def __init__(self):
+        """
+        スコア表示用の文字列Surfaceを生成する
+        """
+        self.fonto = pg.font.Font(None, 50)  # フォントの設定
+        self.color = (0, 0, 255)             # 文字色：青
+        self.score = 0                       # スコアの初期値
+
+        self.img = self.fonto.render(f"Score: {self.score}", 0, self.color)
+        self.rct = self.img.get_rect()
+        self.rct.center = (100, HEIGHT-50)   # 画面左下あたり
+
+    def update(self, screen: pg.Surface):
+        """
+        現在のスコアを画面に表示する
+        """
+        self.img = self.fonto.render(f"Score: {self.score}", 0, self.color)
+        screen.blit(self.img, self.rct)
 
 
 def main():
@@ -145,8 +169,17 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
-    bomb = Bomb((255, 0, 0), 10)
+    # bomb = Bomb((255, 0, 0), 10)
+    # boms = []
+    # for _ in NUM_OF_BOMBS:
+    #     bomb = Bomb((255, 0, 0), 10)
+    #     bombs.append(bomb)
+        
+    bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
+    
     beam = None  # ゲーム初期化時にはビームは存在しない
+    
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -158,31 +191,35 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
+        for bomb in bombs:
+            if bird.rct.colliderect(bomb.rct):
             # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            fonto = pg.font.Font(None, 80)
-            txt = fonto.render("Game Over", True,(255,0,0))
-            screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
-            pg.display.update()
-            time.sleep(1)
-            return
+               bird.change_img(8, screen)
+               fonto = pg.font.Font(None, 80)
+               txt = fonto.render("Game Over", True,(255,0,0))
+               screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
+               pg.display.update()
+               time.sleep(1)
+               return
 
-        if beam is not None:
-            if bomb is not None:
+        for i, bomb in enumerate(bombs):
+            if beam is not None:
                 if beam.rct.colliderect(bomb.rct):
+                    score.score +=1
                     bird.change_img(6, screen)
                     pg.display.update()
                     beam = None
-                    bomb = None
-            
+                    bombs[i] = None
+        bombs = [bomb for bomb in bombs if bomb is not None]
+           
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
             beam.update(screen) 
-        if beam is not None:  
+        for bomb in bombs:  
            bomb.update(screen)
+           score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
